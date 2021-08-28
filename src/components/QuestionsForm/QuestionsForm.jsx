7baa74cdb69f1ebd0813620336e47d55d457551c
israelpeanut <israel.lazo@externos-cl.cencosud.com>
2021-08-28 01:04:57 -0400

    questions form dynamic json and validation

--- src/components/QuestionsForm/QuestionsForm.jsx
+++ src/components/QuestionsForm/QuestionsForm.jsx
@@ -9,54 +9,90 @@ import {
 import questions from '../../data/questions.json';
 
 export const QuestionsForm = () => {
-  const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState(true);
+  const [profile, setProfile] = useState({...questions.fullstack})
+  const [answers, setAnswers] = useState({
+    questions:[],
+    long_question: {question:profile.long_question, answer: ""},
+  })
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+    console.log(form.checkValidity())
+    event.preventDefault();
+    event.stopPropagation();
+    
+    if (!form.checkValidity()) {
+      
     }
+    alert("enviando form");
+    setValidated(true);
+  }
+
+  const onChangeRadio = (selected, question) => {
+    const tempAnswers = {...answers};
+    const item = tempAnswers.questions.find(x => x.question === question)
+
+    if(item)
+      item.answer = selected
+    else
+      tempAnswers.questions.push({question:question, answer:selected})
+    
+    setAnswers({answers, ...tempAnswers});
+    console.log(answers);
+  }
+
+  const onChangeText = (event) => {
+    const tempAnswers = {...answers};
+    tempAnswers.long_question.answer = event.target.value;
+    
+    setAnswers({answers, ...tempAnswers});
+    console.log(answers);
   }
 
   return (
     <div className="p-3">
       <h4>Postula a este trabajo</h4>
     
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form noValidate validated={validated} onSubmit={handleSubmit} className="needs-validation">
+        {profile.questions.map((question) => (
+          <Row className="mb-3" key={question.id}>
+            <Form.Group as={Col} md="4">
+              <div className="question-title">{question.title} <span className="error">*</span></div>
+                {question.alternatives.map((alternative) => (
+                  <Form.Check
+                    key={`${question.id}${alternative.id}`}
+                    type='radio'
+                    label={alternative.text}
+                    name={question.title}
+                    id={`disabled-default-radio`}
+                    className="question-option"
+                    onChange={(e) => onChangeRadio(e.target.value, question.title)}
+                    value={alternative.text}
+                    required
+                  />
+                ))}
+              {!validated && <div className="error error-label">Hey! No te olvides de esto!</div>}
+            </Form.Group>
+          </Row>
+        ))}
         <Row className="mb-3">
           <Form.Group as={Col} md="4" controlId="validationCustom01">
-            <div className="question-title">¿Tangananica o Tangananá? <span className="error">*</span></div>
-            <Form.Check
-              type='radio'
-              label={`radio 1`}
-              name='pregunta1'
-              id={`disabled-default-radio`}
-              className="question-option"
+            <div className="question-title">{profile.long_question} <span className="error">*</span></div>
+            <FormControl
+              as="textarea"
+              aria-label="With textarea"
+              value={answers.long_question.answer}
+              onChange={onChangeText}
               required
             />
-            <Form.Check
-              type='radio'
-              label={`radio 2`}
-              name='pregunta1'
-              id={`disabled-default-radio`}
-              required
-            />
-            <div className="error error-label">Hey! No te olvides de esto!</div>
-          </Form.Group>
-        </Row>
-        
-        <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom01">
-            <p>¿Qué impacto crees que tiene la tecnología en el mundo?</p>
-            <FormControl as="textarea" aria-label="With textarea" />
+            {!validated && <div className="error error-label">Hey! No te olvides de esto!</div>}
           </Form.Group>
         </Row>
 
         <Row className="mb-3">
         <Button
           type="submit"
-          disabled={!validated}
           className="peanut-button"
           >Enviar</Button>
         </Row>
